a9cb1b2db0a9209ec6f3ebc51b7345eae2d4cd42
Sam Zhang <sam.zijing.zhang@gmail.com>
2024-10-02 11:57:50 +0800

    template camera page

--- src/pages/CameraPage.tsx
+++ src/pages/CameraPage.tsx
@@ -1,5 +1,17 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+
 const CameraPage = () => {
-  return <div>This is the Camera Page</div>;
+  return (
+    <div className="flex justify-center px-40 flex-col gap-10">
+      <div className="border border-muted aspect-video h-2/3 text-center">
+        Camera will be displayed here
+      </div>
+
+      <ScrollArea className="border border-muted rounded-lg h-40 p-4 font-mono">
+        Logs will be displayed here
+      </ScrollArea>
+    </div>
+  );
 };
 
 export default CameraPage;
